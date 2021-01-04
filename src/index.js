448ee80fb51ed1b5f71b8325769b4c8166e82eae
Sera_Is <isonserena@hotmail.it>
2021-01-04 15:55:01 +0100

    Changed API urls from HTTP to HTTPS
    loading all resources via HTTPS to improve security of the site

--- src/index.js
+++ src/index.js
@@ -1,29 +1,33 @@
 function displayWeather(response) {
   document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temp").innerHTML = Math.round(response.data.main.temp);
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
-  document.querySelector("#description").innerHTML = response.data.weather[0].main;
-};
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
 
 function fetchCity(city) {
   let metric = "metric";
   let apiKey = `9aec27109595b5fdde1289ca7baf818f`;
-  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${metric}`;
-  
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${metric}`;
+
   axios.get(apiUrl).then(displayWeather);
 }
 
 function inputCity(event) {
-event.preventDefault();
+  event.preventDefault();
   let city = document.querySelector("#search-input").value;
   fetchCity(city);
-  }
+}
 let cityName = document.querySelector("#search-form");
 cityName.addEventListener("submit", inputCity);
 
 fetchCity("Amsterdam");
-
 
 function showPosition(position) {
   let latitude = position.coords.latitude;
@@ -35,47 +39,68 @@ function showPosition(position) {
   axios.get(url).then(displayWeather);
 }
 function displayCurrentCityWeahter(event) {
-event.preventDefault();
-navigator.geolocation.getCurrentPosition(showPosition);
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
 }
 
-let currentCity = document.querySelector("#current-city")
-currentCity.addEventListener("click", displayCurrentCityWeahter)
+let currentCity = document.querySelector("#current-city");
+currentCity.addEventListener("click", displayCurrentCityWeahter);
 
 function convertToF(event) {
-    event.preventDefault();
-    let celsius = 16;
-    let fahrenheit = Math.round(celsius * 9/5 + 32);
-    let h2 = document.querySelector("h2");
-    h2.innerHTML = `${fahrenheit}`;
+  event.preventDefault();
+  let celsius = 16;
+  let fahrenheit = Math.round((celsius * 9) / 5 + 32);
+  let h2 = document.querySelector("h2");
+  h2.innerHTML = `${fahrenheit}`;
 }
 let unitF = document.querySelector("#Fahrenheit");
 unitF.addEventListener("click", convertToF);
 
 function convertToC(event) {
-    event.preventDefault();
-    let celsius = 16;
-    let h2 = document.querySelector("h2");
-    h2.innerHTML = `${celsius}`;
+  event.preventDefault();
+  let celsius = 16;
+  let h2 = document.querySelector("h2");
+  h2.innerHTML = `${celsius}`;
 }
 let unitC = document.querySelector("#Celsius");
 unitC.addEventListener("click", convertToC);
 
 function formatDate(dateFormatted) {
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let day = days[now.getDay()];
-    let hours = now.getHours();
-    if (hours < 10) {
-      hours = `0${hours}`;
-    };
-    let minutes = now.getMinutes();
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    };
-    let date = now.getDate();
-    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    let month = months[now.getMonth()];
-return `${day}, ${month} ${date}, ${hours}:${minutes}`;
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let date = now.getDate();
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[now.getMonth()];
+  return `${day}, ${month} ${date}, ${hours}:${minutes}`;
 }
 
 let weatherDate = document.querySelector("#date");
